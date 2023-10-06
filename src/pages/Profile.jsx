b18650ad0790b67b1a54364dbaca11a1a0a923f6
AiXin18 <91781056+AiXin18@users.noreply.github.com>
2023-10-07 00:01:31 +0800

    validations for myprofilepage update (forgot to select it)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import profilepicture from "../assets/profilepicture.jpg";
+import defaultprofilepic from "../assets/defaultprofilepic.jpg";
 import qr from "../assets/QR/qr.avif";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
@@ -15,6 +15,7 @@ import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Collapse from "@mui/material/Collapse";
 import Alert from "@mui/material/Alert";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Profile() {
   const gridItemStyle = {
@@ -51,12 +52,173 @@ function Profile() {
     setSelectedButton(button);
   };
 
+  // email and phone number validation
+  // State variables for email and phone number
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState("");
+
+  // Function to validate email
+  const validateEmail = () => {
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    if (!email.match(emailRegex)) {
+      setEmailError("Invalid Email");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  // Function to validate phone number
+  const validatePhoneNumber = () => {
+    const phoneRegex = /^[89]\d{7}$/;
+    if (!phoneNumber.match(phoneRegex)) {
+      setPhoneNumberError("Invalid Phone Number");
+    } else {
+      setPhoneNumberError("");
+    }
+  };
+
+  // Function to handle input changes
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  // State variables for password and confirm password
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmedPasswordError, setConfirmedPasswordError] = useState("");
+
+  // Function to validate password
+  const validatePassword = () => {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%^&*()\-_+={}[\]|:;"<>,./?])[A-Za-z\d~`!@#$%^&*()\-_+={}[\]|:;"<>,./?]{10,}$/;
+    if (!passwordRegex.test(password)) {
+      setPasswordError("Password must meet the specified criteria.");
+    } else if (password.length < 12) {
+      setPasswordError("Password must be at least 12 characters");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  // Function to validate confirmed password
+  const validateConfirmedPassword = () => {
+    if (password !== confirmedPassword) {
+      setConfirmedPasswordError("Passwords do not match");
+    } else {
+      setConfirmedPasswordError("");
+    }
+  };
+
+  // Function to handle input changes
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmedPasswordChange = (event) => {
+    setConfirmedPassword(event.target.value);
+  };
+
+  // when button clicked
+  const handleSave_emailphoneno = () => {
+    // Validate email, phone number
+    const isEmailValid = validateEmail();
+    const isPhoneNumberValid = validatePhoneNumber();
+
+    // Initialize an array to collect error messages
+    const errorMessages = [];
+
+    // Check email validation
+    if (!isEmailValid) {
+      errorMessages.push("Invalid Email");
+    }
+
+    // Check phone number validation
+    if (!isPhoneNumberValid) {
+      errorMessages.push("Invalid Phone Number");
+    }
+
+    if (errorMessages.length > 0) {
+      // If there are error messages, display them in the Alert component
+      setErrorMessages(errorMessages);
+    }
+  };
+
+  const handleSave_password = () => {
+    // Validate password, and confirmed password
+    const isPasswordValid = validatePassword();
+    const isConfirmedPasswordValid = validateConfirmedPassword();
+
+    // Initialize an array to collect error messages
+    const errorMessages = [];
+
+    // Check password validation
+    if (!isPasswordValid) {
+      errorMessages.push("Invalid Password");
+    }
+
+    // Check confirmed password validation
+    if (!isConfirmedPasswordValid) {
+      errorMessages.push("Passwords do not match");
+    }
+
+    if (errorMessages.length > 0) {
+      // If there are error messages, display them in the Alert component
+      setErrorMessages(errorMessages);
+    }
+  };
+
+  const handleSave_profilepicture = () => {
+    // save the profile into the database
+  };
+
+  // profile picture
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  // Function to handle file selection
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      // Check if the selected file is an image or GIF
+      if (file.type.startsWith("image/") || file.type === "image/gif") {
+        setSelectedFile(file);
+      } else {
+        // Invalid file type, you can display an error message here
+        console.error("Invalid file type. Please select an image or GIF.");
+      }
+    }
+  };
+
+  // Function to remove the selected image
+  const handleRemoveImage = () => {
+    setSelectedFile(null); // Clear the selectedFile state to remove the image
+  };
+
   return (
     <div>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={4} style={gridItemStyle}>
           <Paper elevation={3}>
-            <img src={profilepicture} style={{ width: "100%" }} />
+            {selectedFile ? (
+              <img
+                src={URL.createObjectURL(selectedFile)}
+                style={{ width: "100%" }}
+                alt="Profile Pic"
+              ></img>
+            ) : (
+              <img
+                src={defaultprofilepic}
+                style={{ width: "100%" }}
+                alt="Profile Pic"
+              ></img>
+            )}
+
             <div style={{ padding: "15px", paddingLeft: "25px" }}>
               <div style={{ display: "flex" }}>
                 <div
@@ -80,7 +242,7 @@ function Profile() {
                   </Typography>
                 </div>
                 <div style={{ flex: 1, textAlign: "center" }}>
-                  <Typography>
+                  <Typography style={{ marginTop: "15px" }}>
                     <b>Scan Me: 2FA</b>
                   </Typography>
                   <img src={qr} style={{ width: "100%" }} />
@@ -92,49 +254,46 @@ function Profile() {
         <div style={{ padding: "10px", paddingLeft: "20px" }}>
           <h1>My Profile</h1>
 
-          <div style={{ marginTop: "10px", marginBottom: "30px" }}>
-            <Alert severity="error">Error: Account Failed to Update.</Alert>
-            <Alert severity="success">Account Updated Successfully!</Alert>
+          <div style={{ marginTop: "40px", marginBottom: "0px" }}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              style={{
+                marginRight: "15px",
+                backgroundColor: selectedButton === "edit" ? "purple" : "white",
+                color: selectedButton === "edit" ? "white" : "black",
+              }}
+              onClick={() => handleButtonClick("edit")}
+            >
+              Edit My Account
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              style={{
+                marginRight: "15px",
+                backgroundColor:
+                  selectedButton === "updatePic" ? "purple" : "white",
+                color: selectedButton === "updatePic" ? "white" : "black",
+              }}
+              onClick={() => handleButtonClick("updatePic")}
+            >
+              Update Profile Picture
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              style={{
+                marginRight: "15px",
+                backgroundColor:
+                  selectedButton === "changePass" ? "purple" : "white",
+                color: selectedButton === "changePass" ? "white" : "black",
+              }}
+              onClick={() => handleButtonClick("changePass")}
+            >
+              Change Password
+            </Button>
           </div>
-
-          <Button
-            variant="outlined"
-            color="secondary"
-            style={{
-              marginRight: "15px",
-              backgroundColor: selectedButton === "edit" ? "purple" : "white",
-              color: selectedButton === "edit" ? "white" : "black",
-            }}
-            onClick={() => handleButtonClick("edit")}
-          >
-            Edit My Account
-          </Button>
-          <Button
-            variant="outlined"
-            color="secondary"
-            style={{
-              marginRight: "15px",
-              backgroundColor:
-                selectedButton === "updatePic" ? "purple" : "white",
-              color: selectedButton === "updatePic" ? "white" : "black",
-            }}
-            onClick={() => handleButtonClick("updatePic")}
-          >
-            Update Profile Picture
-          </Button>
-          <Button
-            variant="outlined"
-            color="secondary"
-            style={{
-              marginRight: "15px",
-              backgroundColor:
-                selectedButton === "changePass" ? "purple" : "white",
-              color: selectedButton === "changePass" ? "white" : "black",
-            }}
-            onClick={() => handleButtonClick("changePass")}
-          >
-            Change Password
-          </Button>
 
           <Collapse in={selectedButton === "edit"} timeout="auto" unmountOnExit>
             <h3 style={{ marginTop: "50px" }}>Update Account Details</h3>
@@ -146,7 +305,15 @@ function Profile() {
                     id="emailAddress"
                     type="text"
                     placeholder="Enter Email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    onBlur={validateEmail}
                   />
+                  {emailError && (
+                    <Alert severity="error" style={{ marginBottom: "10px" }}>
+                      {emailError}
+                    </Alert>
+                  )}
                 </FormControl>
               </div>
 
@@ -157,9 +324,33 @@ function Profile() {
                     id="phoneNumber"
                     type="text"
                     placeholder="Enter Phone Number"
+                    value={phoneNumber}
+                    onChange={handlePhoneNumberChange}
+                    onBlur={validatePhoneNumber}
                   />
+                  {phoneNumberError && (
+                    <Alert severity="error" style={{ marginBottom: "10px" }}>
+                      {phoneNumberError}
+                    </Alert>
+                  )}
                 </FormControl>
               </div>
+            </div>
+            <div style={{ marginTop: "30px" }}>
+              <Button
+                variant="outlined"
+                size="medium"
+                style={{ marginRight: "20px" }}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                size="medium"
+                onClick={handleSave_emailphoneno}
+              >
+                Save
+              </Button>
             </div>
           </Collapse>
 
@@ -171,18 +362,49 @@ function Profile() {
           >
             <h3 style={{ marginTop: "50px" }}>Update Profile Picture</h3>
             <div style={{ display: "flex", alignItems: "center" }}>
+              <p style={{ marginRight: "20px" }}>Profile Picture:</p>
               <Button
                 style={{ backgroundColor: "grey" }}
                 component="label"
                 variant="contained"
                 startIcon={<CloudUploadIcon />}
               >
-                Upload file
-                <VisuallyHiddenInput type="file" />
+                Upload a file
+                <input
+                  type="file"
+                  style={{ display: "none" }}
+                  accept="image/*,.gif"
+                  onChange={handleFileChange}
+                />
               </Button>
-              <p style={{ marginLeft: "20px", color: "blue" }}>
-                myprofilepic.jpg
+              {selectedFile && ( // Display the "Remove" button if an image is selected
+                <Button
+                  style={{ backgroundColor: "red", marginLeft: "20px" }}
+                  variant="contained"
+                  onClick={handleRemoveImage}
+                >
+                  <DeleteIcon />
+                </Button>
+              )}
+            </div>
+            <div>
+              <p>File Name: </p>
+              <p style={{ color: "blue" }}>
+                {selectedFile ? selectedFile.name : "No file selected"}
               </p>
+            </div>
+            <div style={{ marginTop: "30px" }}>
+              <Button
+                variant="outlined"
+                size="medium"
+                style={{ marginRight: "20px" }}
+                onClick={handleRemoveImage}
+              >
+                Cancel
+              </Button>
+              <Button variant="contained" size="medium">
+                Save
+              </Button>
             </div>
           </Collapse>
 
@@ -201,6 +423,9 @@ function Profile() {
                     id="outlined-adornment-password"
                     type={showPassword ? "text" : "password"}
                     placeholder="Enter Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    onBlur={validatePassword}
                     endAdornment={
                       <InputAdornment position="end">
                         <IconButton
@@ -214,6 +439,11 @@ function Profile() {
                       </InputAdornment>
                     }
                   />
+                  {passwordError && (
+                    <Alert severity="error" style={{ marginBottom: "10px" }}>
+                      {passwordError}
+                    </Alert>
+                  )}
                 </FormControl>
               </div>
 
@@ -224,10 +454,13 @@ function Profile() {
                     id="outlined-adornment-confirmed-password"
                     type={showPassword2 ? "text" : "password"}
                     placeholder="Enter Confirmed Password"
+                    value={confirmedPassword}
+                    onChange={handleConfirmedPasswordChange}
+                    onBlur={validateConfirmedPassword}
                     endAdornment={
                       <InputAdornment position="end">
                         <IconButton
-                          aria-label
+                          aria-label="toggle password visibility"
                           onClick={handleClickShowPassword2}
                           onMouseDown={handleMouseDownPassword2}
                           edge="end"
@@ -237,24 +470,31 @@ function Profile() {
                       </InputAdornment>
                     }
                   />
+                  {confirmedPasswordError && (
+                    <Alert severity="error" style={{ marginBottom: "10px" }}>
+                      {confirmedPasswordError}
+                    </Alert>
+                  )}
                 </FormControl>
               </div>
             </div>
+            <div style={{ marginTop: "30px" }}>
+              <Button
+                variant="outlined"
+                size="medium"
+                style={{ marginRight: "20px" }}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                size="medium"
+                onClick={handleSave_password}
+              >
+                Save
+              </Button>
+            </div>
           </Collapse>
-
-          {/* buttons add here */}
-          <div style={{ marginTop: "30px" }}>
-            <Button
-              variant="outlined"
-              size="medium"
-              style={{ marginRight: "20px" }}
-            >
-              Cancel
-            </Button>
-            <Button variant="contained" size="medium">
-              Save
-            </Button>
-          </div>
         </div>
       </Grid>
     </div>
