26ece6a7ea17be26659dc40bb306533a66d2bcad
sachinagrawal19 <sachout1999@gmail.com>
2023-11-02 22:41:22 +0800

    Create SessionExpired.test.js

---
+++ src/test/SessionExpired.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import SessionExpired from "../pages/SessionExpired"; 
+import { MemoryRouter, useNavigate } from 'react-router-dom'; // Import MemoryRouter
+
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useNavigate: jest.fn(),
+  }));
+
+describe("SessionExpired Component", () => {
+  it("Renders SessionExpired component", () => {
+    render(<MemoryRouter>
+      <SessionExpired />
+    </MemoryRouter>);
+    
+    // Check if the main title is present
+    const titleElement = screen.getByText("Session Expired");
+    expect(titleElement).toBeTruthy();
+
+    // Check if the session expired message is present
+    const messageElement = screen.getByText("OOPS! Your session have been expired.");
+    expect(messageElement).toBeTruthy();
+
+    // Check if the "Go to Login" button is present
+    const loginButton = screen.getByText("Go to Login");
+    expect(loginButton).toBeTruthy();
+  });
+
+  it("Clicking the 'Go to Login' button navigates to the login page", () => {
+    const mockNavigate = jest.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+
+    render(
+      <MemoryRouter>
+        <SessionExpired />
+      </MemoryRouter>
+    );
+
+    const loginButton = screen.getByText("Go to Login");
+    fireEvent.click(loginButton);
+
+    expect(mockNavigate).toHaveBeenCalledWith("/");
+  });
+});
